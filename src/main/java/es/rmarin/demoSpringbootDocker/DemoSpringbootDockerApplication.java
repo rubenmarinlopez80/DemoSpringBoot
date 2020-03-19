f42069bbeeb12cb18cafd969a995ddcfcbd8a66a
Ruben <Ruben@DESKTOP-909TTQ9>
2020-03-20 00:38:41 +0100

    v1.3

--- src/main/java/es/rmarin/demoSpringbootDocker/DemoSpringbootDockerApplication.java
+++ src/main/java/es/rmarin/demoSpringbootDocker/DemoSpringbootDockerApplication.java
@@ -1,13 +1,24 @@
 package es.rmarin.demoSpringbootDocker;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class DemoSpringbootDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringbootDockerApplication.class, args);
 	}
+	
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoSpringbootDockerApplication.class);
+    }
 
 }
